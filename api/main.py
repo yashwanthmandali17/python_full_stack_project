17f21023af097cd78192b23e78bfa6247b15603b
Mandali Yashwanth <your_github_email@example.com>
2025-10-07 21:16:48 +0530

    final commit

--- api/main.py
+++ api/main.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 from typing import Optional, List
 import os
 import sys
-
 # Add the parent directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
